5b0007a659ec4215d461bb688ec73deebf690236
MwasCipher <mwangicollinswaweru@gmail.com>
2019-06-17 16:17:35 +0300

    Initial Commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,197 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+    <script type="text/javascript"src= "<?php echo base_url('assets/js/bootstrap.bundle.min.js')?> "> </script>
+    <script type="text/javascript"src= "<?php echo base_url('assets/js/bootstrap.min.js')?>"> </script>
+
+    <link rel="stylesheet" style="text/css" href="<?php echo base_url('assets/css/bootstrap.css')?>" >
+
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">CRUD With Bootstrap</a>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+
+    <section id="content">
+        <div class="container">
+            <!-- Default table -->
+            <div class="row">
+                <div class="span6">
+                    <h4>Default styles</h4>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>
+                                First Name
+                            </th>
+                            <th>
+                                Last Name
+                            </th>
+                            <th>
+                                Username
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>
+                                1
+                            </td>
+                            <td>
+                                Mark
+                            </td>
+                            <td>
+                                Otto
+                            </td>
+                            <td>
+                                @mdo
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                2
+                            </td>
+                            <td>
+                                Jacob
+                            </td>
+                            <td>
+                                Thornton
+                            </td>
+                            <td>
+                                @fat
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                3
+                            </td>
+                            <td>
+                                Larry
+                            </td>
+                            <td>
+                                the Bird
+                            </td>
+                            <td>
+                                @twitter
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="span6">
+                    <h4>Default Styles with Zebra-Striping</h4>
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>
+                                First Name
+                            </th>
+                            <th>
+                                Last Name
+                            </th>
+                            <th>
+                                Username
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>
+                                1
+                            </td>
+                            <td>
+                                Mark
+                            </td>
+                            <td>
+                                Otto
+                            </td>
+                            <td>
+                                @mdo
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                2
+                            </td>
+                            <td>
+                                Jacob
+                            </td>
+                            <td>
+                                Thornton
+                            </td>
+                            <td>
+                                @fat
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                3
+                            </td>
+                            <td>
+                                Larry
+                            </td>
+                            <td>
+                                the Bird
+                            </td>
+                            <td>
+                                @twitter
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- divider -->
+            <div class="row">
+                <div class="span12">
+                    <div class="solidline">
+                    </div>
+                </div>
+            </div>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">CRUD With Bootstrap</a>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+
+
+</head>
+<body>
+
+
+
+</body>
+</html>
